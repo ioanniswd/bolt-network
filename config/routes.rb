e46082ed59b4d257930e16376feef2456764b1b1
ioanniswd <ioanniswd@gmail.com>
2019-01-12 02:39:34 +0200

    Added pages controller. Not done. Css not working, about not done.
     On branch master
    
     Changes to be committed:
    	new file:   app/assets/javascripts/pages.coffee
    	new file:   app/assets/stylesheets/pages.css.scss
    	new file:   app/assets/stylesheets/pages.scss
    	new file:   app/controllers/pages_controller.rb
    	new file:   app/helpers/pages_helper.rb
    	new file:   app/views/pages/home.html.erb
    	modified:   config/routes.rb
    	new file:   test/controllers/pages_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/pages/home' => 'pages#home'
+  get '/pages/about' => 'pages#about'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
