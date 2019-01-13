11b10865a3884ea4c77862b3d304b4207c92cf99
ioanniswd <ioanniswd@gmail.com>
2019-01-13 11:54:36 +0200

    Added about page, and css in layout.
     On branch master
    
     Changes to be committed:
    	modified:   app/views/layouts/application.html.erb
    	new file:   app/views/pages/about.html.erb
    	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/pages/home' => 'pages#home'
   get '/pages/about' => 'pages#about'
+  get '/about' => 'pages#about'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
